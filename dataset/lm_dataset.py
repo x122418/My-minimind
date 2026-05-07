8df131923fa6b22e1341508bf19b1fac88a8e40c
night <hjq122418@163.com>
2026-05-07 15:40:11 +0800

    ‘finish something about dataset’

--- dataset/lm_dataset.py
+++ dataset/lm_dataset.py
@@ -21,6 +21,30 @@ class PretrainDateset(Dataset):
     # __len__
     def __len__(self):
         return len(self.samples)
-
     # __getitem__
-
+    def __getitem__(self, index:int):
+        
+    # 拿到json中的每一行
+        sample = self.samples[index]
+    # tokenizer转化字符串为token_id
+        tokens = self.tokenizer((sample['text']), 
+                                add_special_tokens = False,
+                                max_length = self.max_length - 2,
+                                truncation = True).input_ids #假设每一行都有text字段 表示文本
+    # 加入PAD，EOS，BOS
+        tokens = [self.tokenizer.bos_token_id] + tokens + [self.tokenizer.eos_token_id]
+        input_ids = tokens + [self.tokenizer.pad_token_id] * (self.max_length - len(tokens))
+        input_ids = torch.Tensor(input_ids, dtype = torch.long)
+    
+    # 自行编写labels, prevent pad being computed in loss
+        labels = input_ids.clone()
+        labels = tokens[tokens == self.tokenizer.pad_token_id] = -100
+    # 编写attn_mask, 标记有效位置和PAD  (非PAD为1 PAD为0)
+        attn_mask = (input_ids != self.tokenizer.pad_token_id).long()
+    # 返回input_id, attn_mask, labels
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attn_mask,
+            'labels': labels
+        }
+        
